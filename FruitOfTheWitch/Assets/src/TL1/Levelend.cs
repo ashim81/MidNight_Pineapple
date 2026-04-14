a37e35c1ac465b6f4748496184d1d226a5c82c95
ashim81 <178589628+ashim81@users.noreply.github.com>
2026-04-14 00:49:13 -0700

    level1 extend recovered

---
+++ FruitOfTheWitch/Assets/src/TL1/Levelend.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LevelManager.instance.NextLevel();
+        }
+    }
+}
